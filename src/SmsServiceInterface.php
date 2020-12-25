2fed4988f55b15d08cf6a294df5c7e24804d546e
小龙 <864566775@qq.com>
2020-12-25 11:47:41 +0800

    Update SmsServiceInterface.php

--- src/SmsServiceInterface.php
+++ src/SmsServiceInterface.php
@@ -1,11 +1,6 @@
 <?php
 namespace longxiaoyang\smsServiceInterface;
 
-/**
- * Class SmsService
- * @package App\JsonRpc
- * @RpcService(name="SmsService", protocol="jsonrpc-http", server="jsonrpc-http", publishTo="consul")
- */
 interface SmsServiceInterface
 {
     public function send(string $mobile, int $type);
